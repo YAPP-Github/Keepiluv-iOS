f4d748f5bad12392d7c39ec290a416750c5c86c5
Jiyong Jung <hickerd@sch.ac.kr>
2025-12-26 14:04:07 +0900

    feat: integrate tuist and fastlane match

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -6,7 +6,15 @@ let project = Project(
     targets: [
         .app(
             implements: .iOS,
-            config: .init()
+            config: .init(
+                settings: .settings(
+                    base: [
+                        "CODE_SIGN_STYLE": "Manual",
+                        "DEVELOPMENT_TEAM": "VZC79KP79S",
+                        "PROVISIONING_PROFILE_SPECIFIER": "match Development org.yapp.twix"
+                    ]
+                )
+            )
         )
     ]
 )
