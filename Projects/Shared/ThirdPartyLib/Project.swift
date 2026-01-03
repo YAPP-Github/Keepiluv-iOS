f4281c46bcef8942546059a593f5acd8877a746c
jihun <jihun332@gmai.com>
2026-01-03 23:15:24 +0900

    feat: SharedThirdPartyLib 모듈 추가 - #30

---
+++ Projects/Shared/ThirdPartyLib/Project.swift
@@ -0,0 +1,12 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+    name: Module.Shared.name + Module.Shared.thirdPartyLib.rawValue,
+    targets: [    
+        .shared(
+            implements: .thirdPartyLib,
+            config: .init()
+        )
+    ]
+)
