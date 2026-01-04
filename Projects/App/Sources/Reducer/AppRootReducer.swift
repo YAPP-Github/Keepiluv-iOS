c2dc6b8a49efca6afbcdfdd63b60ac1be49ad07d
jihun <jihun332@gmai.com>
2026-01-04 16:49:08 +0900

    feat: App 계층 View, Reducer 임시 구현 및 MainTab 연결

---
+++ Projects/App/Sources/Reducer/AppRootReducer.swift
@@ -0,0 +1,30 @@
+//
+//  AppRootReducer.swift
+//  Twix
+//
+//  Created by 정지훈 on 1/4/26.
+//
+
+import ComposableArchitecture
+import Feature
+
+@Reducer
+struct AppRootReducer {
+    
+    @ObservableState
+    struct State: Equatable {
+        var mainTab: MainTabReducer.State = .init()
+        
+        public init() { }
+    }
+    
+    enum Action {
+        case mainTab(MainTabReducer.Action)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Scope(state: \.mainTab, action: \.mainTab) {
+            MainTabReducer()
+        }
+    }
+}
