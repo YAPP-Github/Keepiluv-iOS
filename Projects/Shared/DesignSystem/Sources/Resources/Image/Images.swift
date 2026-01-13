704e09681414684068b930dcf81d43d0d3739cd1
jihun <jihun332@gmai.com>
2026-01-14 08:55:23 +0900

    feat: ImageAsset 추가 - #35 - gitignore에 Icon이 있어서 ImageAsset.assets Icon -> Icons로 변경

--- Projects/Shared/DesignSystem/Sources/Resources/Image/Images.swift
+++ Projects/Shared/DesignSystem/Sources/Resources/Image/Images.swift
@@ -1,5 +1,5 @@
 //
-//  KLIcon.swift
+//  KLIcons.swift
 //  SharedDesignSystem
 //
 //  Created by 정지훈 on 1/13/26.
@@ -17,44 +17,44 @@ public extension Image.Icon {
 }
 
 public extension Image.Icon.Illustration {
-    static let add = SharedDesignSystemAsset.Icon.Illustration.iconAdd.swiftUIImage
-    static let delete = SharedDesignSystemAsset.Icon.Illustration.iconDelete.swiftUIImage
-    static let drug = SharedDesignSystemAsset.Icon.Illustration.iconDrug.swiftUIImage
-    static let emoji1 = SharedDesignSystemAsset.Icon.Illustration.iconEmoji1.swiftUIImage
-    static let emoji2 = SharedDesignSystemAsset.Icon.Illustration.iconEmoji2.swiftUIImage
-    static let emoji3 = SharedDesignSystemAsset.Icon.Illustration.iconEmoji3.swiftUIImage
-    static let emoji4 = SharedDesignSystemAsset.Icon.Illustration.iconEmoji4.swiftUIImage
-    static let emoji5 = SharedDesignSystemAsset.Icon.Illustration.iconEmoji5.swiftUIImage
-    static let emojiAdd = SharedDesignSystemAsset.Icon.Illustration.iconEmojiAdd.swiftUIImage
-    static let exercise = SharedDesignSystemAsset.Icon.Illustration.iconExercise.swiftUIImage
-    static let fire = SharedDesignSystemAsset.Icon.Illustration.iconFire.swiftUIImage
-    static let heart = SharedDesignSystemAsset.Icon.Illustration.iconHeart.swiftUIImage
-    static let success = SharedDesignSystemAsset.Icon.Illustration.iconSuccess.swiftUIImage
+    static let add = SharedDesignSystemAsset.Icons.Illustration.iconAdd.swiftUIImage
+    static let delete = SharedDesignSystemAsset.Icons.Illustration.iconDelete.swiftUIImage
+    static let drug = SharedDesignSystemAsset.Icons.Illustration.iconDrug.swiftUIImage
+    static let emoji1 = SharedDesignSystemAsset.Icons.Illustration.iconEmoji1.swiftUIImage
+    static let emoji2 = SharedDesignSystemAsset.Icons.Illustration.iconEmoji2.swiftUIImage
+    static let emoji3 = SharedDesignSystemAsset.Icons.Illustration.iconEmoji3.swiftUIImage
+    static let emoji4 = SharedDesignSystemAsset.Icons.Illustration.iconEmoji4.swiftUIImage
+    static let emoji5 = SharedDesignSystemAsset.Icons.Illustration.iconEmoji5.swiftUIImage
+    static let emojiAdd = SharedDesignSystemAsset.Icons.Illustration.iconEmojiAdd.swiftUIImage
+    static let exercise = SharedDesignSystemAsset.Icons.Illustration.iconExercise.swiftUIImage
+    static let fire = SharedDesignSystemAsset.Icons.Illustration.iconFire.swiftUIImage
+    static let heart = SharedDesignSystemAsset.Icons.Illustration.iconHeart.swiftUIImage
+    static let success = SharedDesignSystemAsset.Icons.Illustration.iconSuccess.swiftUIImage
 }
 
 public extension Image.Icon.Symbol {
-    static let icAlert = SharedDesignSystemAsset.Icon.Symbol.icAlert.swiftUIImage
-    static let icArrow1MLeft = SharedDesignSystemAsset.Icon.Symbol.icArrow1MLeft.swiftUIImage
-    static let icArrow1MRight = SharedDesignSystemAsset.Icon.Symbol.icArrow1MRight.swiftUIImage
-    static let icArrow1SRight = SharedDesignSystemAsset.Icon.Symbol.icArrow1SRight.swiftUIImage
-    static let icArrow3Left = SharedDesignSystemAsset.Icon.Symbol.icArrow3Left.swiftUIImage
-    static let icArrow3Right = SharedDesignSystemAsset.Icon.Symbol.icArrow3Right.swiftUIImage
-    static let icArrow4 = SharedDesignSystemAsset.Icon.Symbol.icArrow4.swiftUIImage
-    static let icCheck = SharedDesignSystemAsset.Icon.Symbol.icCheck.swiftUIImage
-    static let icCloseM = SharedDesignSystemAsset.Icon.Symbol.icCloseM.swiftUIImage
-    static let icCloseS = SharedDesignSystemAsset.Icon.Symbol.icCloseS.swiftUIImage
-    static let icEdit = SharedDesignSystemAsset.Icon.Symbol.icEdit.swiftUIImage
-    static let icFlash = SharedDesignSystemAsset.Icon.Symbol.icFlash.swiftUIImage
-    static let icMeatball = SharedDesignSystemAsset.Icon.Symbol.icMeatball.swiftUIImage
-    static let icMinus = SharedDesignSystemAsset.Icon.Symbol.icMinus.swiftUIImage
-    static let icPlus = SharedDesignSystemAsset.Icon.Symbol.icPlus.swiftUIImage
-    static let icReturn = SharedDesignSystemAsset.Icon.Symbol.icReturn.swiftUIImage
-    static let icSetting = SharedDesignSystemAsset.Icon.Symbol.icSetting.swiftUIImage
-    static let icTurn = SharedDesignSystemAsset.Icon.Symbol.icTurn.swiftUIImage
-    static let selectedNone1 = SharedDesignSystemAsset.Icon.Symbol.selectedNone1.swiftUIImage
-    static let selectedNone2 = SharedDesignSystemAsset.Icon.Symbol.selectedNone2.swiftUIImage
-    static let selectedNone = SharedDesignSystemAsset.Icon.Symbol.selectedNone.swiftUIImage
-    static let selectedSelected1 = SharedDesignSystemAsset.Icon.Symbol.selectedSelected1.swiftUIImage
-    static let selectedSelected2 = SharedDesignSystemAsset.Icon.Symbol.selectedSelected2.swiftUIImage
-    static let selectedSelected = SharedDesignSystemAsset.Icon.Symbol.selectedSelected.swiftUIImage
+    static let icAlert = SharedDesignSystemAsset.Icons.Symbol.icAlert.swiftUIImage
+    static let icArrow1MLeft = SharedDesignSystemAsset.Icons.Symbol.icArrow1MLeft.swiftUIImage
+    static let icArrow1MRight = SharedDesignSystemAsset.Icons.Symbol.icArrow1MRight.swiftUIImage
+    static let icArrow1SRight = SharedDesignSystemAsset.Icons.Symbol.icArrow1SRight.swiftUIImage
+    static let icArrow3Left = SharedDesignSystemAsset.Icons.Symbol.icArrow3Left.swiftUIImage
+    static let icArrow3Right = SharedDesignSystemAsset.Icons.Symbol.icArrow3Right.swiftUIImage
+    static let icArrow4 = SharedDesignSystemAsset.Icons.Symbol.icArrow4.swiftUIImage
+    static let icCheck = SharedDesignSystemAsset.Icons.Symbol.icCheck.swiftUIImage
+    static let icCloseM = SharedDesignSystemAsset.Icons.Symbol.icCloseM.swiftUIImage
+    static let icCloseS = SharedDesignSystemAsset.Icons.Symbol.icCloseS.swiftUIImage
+    static let icEdit = SharedDesignSystemAsset.Icons.Symbol.icEdit.swiftUIImage
+    static let icFlash = SharedDesignSystemAsset.Icons.Symbol.icFlash.swiftUIImage
+    static let icMeatball = SharedDesignSystemAsset.Icons.Symbol.icMeatball.swiftUIImage
+    static let icMinus = SharedDesignSystemAsset.Icons.Symbol.icMinus.swiftUIImage
+    static let icPlus = SharedDesignSystemAsset.Icons.Symbol.icPlus.swiftUIImage
+    static let icReturn = SharedDesignSystemAsset.Icons.Symbol.icReturn.swiftUIImage
+    static let icSetting = SharedDesignSystemAsset.Icons.Symbol.icSetting.swiftUIImage
+    static let icTurn = SharedDesignSystemAsset.Icons.Symbol.icTurn.swiftUIImage
+    static let selectedNone1 = SharedDesignSystemAsset.Icons.Symbol.selectedNone1.swiftUIImage
+    static let selectedNone2 = SharedDesignSystemAsset.Icons.Symbol.selectedNone2.swiftUIImage
+    static let selectedNone = SharedDesignSystemAsset.Icons.Symbol.selectedNone.swiftUIImage
+    static let selectedSelected1 = SharedDesignSystemAsset.Icons.Symbol.selectedSelected1.swiftUIImage
+    static let selectedSelected2 = SharedDesignSystemAsset.Icons.Symbol.selectedSelected2.swiftUIImage
+    static let selectedSelected = SharedDesignSystemAsset.Icons.Symbol.selectedSelected.swiftUIImage
 }
