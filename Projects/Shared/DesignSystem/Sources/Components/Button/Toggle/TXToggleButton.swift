d9b65d4867bbf435a85a135d330be82eb8523263
jihun <jihun332@gmai.com>
2026-01-15 21:11:52 +0900

    refactor: ToggleSwitch UI 수정

--- Projects/Shared/DesignSystem/Sources/Components/Button/Toggle/TXToggleButton.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TXToggleButton.swift
-//  SharedDesignSystem
-//
-//  Created by 정지훈 on 1/15/26.
-//
-
-import Foundation
--- Projects/Shared/DesignSystem/Sources/Components/Button/Toggle/TXToggleButtonType.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TXToggleButtonType.swift
-//  SharedDesignSystem
-//
-//  Created by 정지훈 on 1/15/26.
-//
-
-import Foundation
