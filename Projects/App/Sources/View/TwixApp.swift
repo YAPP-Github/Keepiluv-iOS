2e86141a41a7204e7e7cd9be9e91a69df67f1740
Jiyong Jung <hickerd@sch.ac.kr>
2026-01-16 13:01:43 +0900

    chore: connect App module with FeatureAuth

--- Projects/App/Sources/View/TwixApp.swift
+++ Projects/App/Sources/View/TwixApp.swift
@@ -1,20 +1,43 @@
+import ComposableArchitecture
+import GoogleSignIn
+import KakaoSDKAuth
+import KakaoSDKCommon
 import SwiftUI
 
-import ComposableArchitecture
+#if DEBUG
+import CoreLogging
+#endif
 
 @main
 struct TwixApp: App {
+    init() {
+        configureKakaoSDK()
+    }
+
     var body: some Scene {
         WindowGroup {
             AppRootView(
                 store: Store(
-                    initialState: AppRootReducer
-                        .State(),
-                    reducer: {
-                        AppRootReducer()
-                    }
-                )
+                    initialState: AppRootReducer.State()
+                ) {
+                    AppRootReducer()
+                }
             )
+            .onOpenURL { url in
+                if AuthApi.isKakaoTalkLoginUrl(url) {
+                    _ = AuthController.handleOpenUrl(url: url)
+                }
+                GIDSignIn.sharedInstance.handle(url)
+            }
         }
+    }
+}
+
+private extension TwixApp {
+    func configureKakaoSDK() {
+        guard let kakaoAppKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String else {
+            fatalError("KAKAO_APP_KEY not found in Info.plist")
+        }
+        KakaoSDK.initSDK(appKey: kakaoAppKey)
     }
 }
