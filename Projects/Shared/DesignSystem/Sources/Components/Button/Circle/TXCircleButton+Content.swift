438b6799324c738f82677390c0a8b59c9e9168df
jihun <jihun332@gmai.com>
2026-01-17 10:09:22 +0900

    refactor: TXShapeButton -> TXCircleButton + Style, Contet로 분리

---
+++ Projects/Shared/DesignSystem/Sources/Components/Button/Circle/TXCircleButton+Content.swift
@@ -0,0 +1,49 @@
+//
+//  TXCircleButton+Content.swift
+//  SharedDesignSystem
+//
+//  Created by 정지훈 on 1/17/26.
+//
+
+import SwiftUI
+
+extension TXCircleButton {
+    /// 원형 버튼의 콘텐츠 타입을 정의합니다.
+    public enum Content {
+        case plus
+        case clear
+    }
+}
+
+extension TXCircleButton.Content {
+    var image: Image {
+        switch self {
+        case .plus:
+            return Image.Icon.Symbol.plus
+            
+        case .clear:
+            return Image.Icon.Symbol.closeS
+        }
+    }
+    
+    var frameSize: CGSize {
+        switch self {
+        case .plus:
+            return CGSize(width: 56, height: 56)
+            
+        case .clear:
+            return CGSize(width: 18, height: 18)
+        }
+    }
+    
+    var imageSize: CGSize {
+        switch self {
+        case .plus:
+            return CGSize(width: 44, height: 44)
+            
+        case .clear:
+            return CGSize(width: 14, height: 14)
+        }
+    }
+    
+}
