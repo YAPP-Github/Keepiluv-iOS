22676bc5b7bc43b38cc867237a45ee8ab5d7e1e0
jihun <jihun332@gmai.com>
2026-01-17 10:08:16 +0900

    feat: ColorStyle 정의 - #45

---
+++ Projects/Shared/DesignSystem/Sources/Resources/Color/ColorStyle.swift
@@ -0,0 +1,37 @@
+//
+//  ColorStyle.swift
+//  SharedDesignSystem
+//
+//  Created by 정지훈 on 1/17/26.
+//
+
+import SwiftUI
+
+public enum ColorStyle {
+    case black
+    case white
+}
+
+extension ColorStyle {
+    var foregroundColor: Color {
+        switch self {
+        case .black:
+            return Color.Common.white
+        case .white:
+            return Color.Gray.gray500
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .black:
+            return Color.Gray.gray500
+        case .white:
+            return Color.Common.white
+        }
+    }
+    
+    var borderColor: Color {
+        return Color.Gray.gray500
+    }
+}
