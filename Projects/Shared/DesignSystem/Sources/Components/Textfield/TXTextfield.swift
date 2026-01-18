f4dbfe9926d1d6b2092c0c309d689dcc2addcc7a
jihun <jihun332@gmai.com>
2026-01-18 10:53:08 +0900

    feat: ColorStyle gray 추가 - #48

---
+++ Projects/Shared/DesignSystem/Sources/Components/Textfield/TXTextfield.swift
@@ -0,0 +1,18 @@
+//
+//  TXTextfield.swift
+//  SharedDesignSystem
+//
+//  Created by 정지훈 on 1/17/26.
+//
+
+import SwiftUI
+
+struct TXTextfield: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TXTextfield()
+}
