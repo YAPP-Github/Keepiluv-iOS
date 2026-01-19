a49e445ee2fd2dabe1b8fd9c8f885b36b55573f6
Jiyong Jung <hickerd@sch.ac.kr>
2026-01-19 21:30:32 +0900

    refactor: update Domain layer dependency structure

--- Projects/Domain/Auth/Project.swift
+++ Projects/Domain/Auth/Project.swift
@@ -8,6 +8,7 @@ let project = Project.makeModule(
             interface: .auth,
             config: .init(
                 dependencies: [
+                    .core(interface: .storage),
                     .external(dependency: .ComposableArchitecture)
                 ]
             )
