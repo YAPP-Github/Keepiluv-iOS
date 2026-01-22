fd9abd927f932d73acfd67f200ad9a9f813131b2
jihun <jihun332@gmai.com>
2026-01-22 23:59:55 +0900

    refactor: 재사용을 위해 CommentCircle View 분리 -#63

---
+++ Projects/Feature/GoalDetail/Sources/ProofPhoto/ProofPhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  ProofPhotoView.swift
+//  FeatureGoalDetail
+//
+//  Created by 정지훈 on 1/22/26.
+//
+
+import SwiftUI
+
+struct ProofPhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProofPhotoView()
+}
