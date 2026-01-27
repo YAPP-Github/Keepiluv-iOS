158919ae8e70bb0a0a9ba573abe6ea082bb4779c
jihun <jihun332@gmai.com>
2026-01-27 19:37:04 +0900

    feat: RootHomeView 구현 - #77

---
+++ Projects/Feature/Home/Interface/Sources/HomeRoute.swift
@@ -0,0 +1,13 @@
+//
+//  HomeRoute.swift
+//  FeatureHomeInterface
+//
+//  Created by 정지훈 on 1/27/26.
+//
+
+import Foundation
+
+public enum HomeRoute: Hashable {
+    case edit
+    case detail
+}
