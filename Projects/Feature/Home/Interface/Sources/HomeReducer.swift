2dd2de0c3d4b2911703411b47f5bbebe79f229dc
jihun <jihun332@gmai.com>
2026-01-27 11:12:33 +0900

    feat: HomeReducer, View UI만 구현 - #77

---
+++ Projects/Feature/Home/Interface/Sources/HomeReducer.swift
@@ -0,0 +1,76 @@
+//
+//  HomeReducer.swift
+//  FeatureHomeInterface
+//
+//  Created by 정지훈 on 1/26/26.
+//
+
+import Foundation
+
+import ComposableArchitecture
+import SharedDesignSystem
+
+/// 홈 화면의 상태와 액션을 관리하는 Reducer입니다.
+///
+/// ## 사용 예시
+/// ```swift
+/// let store = Store(
+///     initialState: HomeReducer.State()
+/// ) {
+///     HomeReducer()
+/// }
+/// ```
+@Reducer
+public struct HomeReducer {
+    let reducer: Reduce<State, Action>
+    
+    @ObservableState
+    /// 홈 화면에서 사용되는 상태 모델입니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// let state = HomeReducer.State()
+    /// ```
+    public struct State {
+        public var cards: [GoalCardItem] = []
+        public var isLoading: Bool = true
+        public var hasCards: Bool { !cards.isEmpty }
+
+        /// 기본 상태를 생성합니다.
+        ///
+        /// ## 사용 예시
+        /// ```swift
+        /// let state = HomeReducer.State()
+        /// ```
+        public init() {
+        }
+    }
+    
+    /// 홈 화면에서 발생 가능한 모든 이벤트를 정의합니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// store.send(.onAppear)
+    /// ```
+    public enum Action {
+        // MARK: - LifeCycle
+        case onAppear
+        
+        // MARK: - Update State
+        case fetchGoalsCompleted([GoalCardItem])
+    }
+    
+    /// 외부에서 주입한 Reduce로 HomeReducer를 구성합니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// let reducer = HomeReducer(reducer: Reduce { _, _ in .none })
+    /// ```
+    public init(reducer: Reduce<State, Action>) {
+        self.reducer = reducer
+    }
+    
+    public var body: some ReducerOf<Self> {
+        reducer
+    }
+}
