22436a6c18ad68692a2e598ad6b25e7e7508c360
Jiyong Jung <hickerd@sch.ac.kr>
2026-02-04 22:16:05 +0900

    refactor: cleanup network and onboarding code - #92

--- Projects/Domain/Onboarding/Interface/Sources/Entity/OnboardingStatus.swift
+++ Projects/Domain/Onboarding/Interface/Sources/Entity/OnboardingStatus.swift
@@ -8,7 +8,7 @@ import Foundation
 /// 온보딩 진행 상태를 나타내는 열거형입니다.
 public enum OnboardingStatus: String, Equatable, Sendable, Codable {
     case coupleConnection = "COUPLE_CONNECTION"
-    case profile = "PROFILE"
-    case anniversary = "ANNIVERSARY"
+    case profileSetup = "PROFILE_SETUP"
+    case anniversarySetup = "ANNIVERSARY_SETUP"
     case completed = "COMPLETED"
 }
