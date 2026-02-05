584044976b0d7e8b6e36a21417ac7f15fc62466c
jihun <jihun332@gmai.com>
2026-02-05 10:40:11 +0900

    feat: EditGoalView UI 구현 - #107

--- Projects/Feature/Home/Interface/Sources/Goal/EditGoalReducer.swift
+++ Projects/Feature/Home/Interface/Sources/Goal/EditGoalReducer.swift
@@ -8,25 +8,72 @@
 import Foundation
 
 import ComposableArchitecture
+import SharedDesignSystem
+import SharedUtil
+import SwiftUI
 
 @Reducer
+/// 목표 편집 화면의 상태와 액션을 관리하는 Reducer입니다.
+///
+/// ## 사용 예시
+/// ```swift
+/// let store = Store(initialState: EditGoalReducer.State()) {
+///     EditGoalReducer()
+/// }
+/// ```
 public struct EditGoalReducer {
     
     let reducer: Reduce<State, Action>
     
     @ObservableState
+    /// 목표 편집 화면에서 사용하는 상태 모델입니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// let state = EditGoalReducer.State()
+    /// ```
     public struct State: Equatable {
         
+        public var calendarDate: TXCalendarDate
+        public var calendarWeeks: [[TXCalendarDateItem]]
+        public var cards: [GoalEditCardItem]
         
+        /// 기본 상태를 생성합니다.
+        ///
+        /// ## 사용 예시
+        /// ```swift
+        /// let state = EditGoalReducer.State()
+        /// ```
         public init() {
-            
+            let nowDate = CalendarNow()
+            let today = TXCalendarDate(
+                year: nowDate.year,
+                month: nowDate.month,
+                day: nowDate.day
+            )
+            self.calendarDate = today
+            self.calendarWeeks = TXCalendarDataGenerator.generateWeekData(for: today)
+            self.cards = []
         }
     }
     
+    /// 목표 편집 화면에서 발생 가능한 이벤트입니다.
     public enum Action {
+        // MARK: - LifeCycle
+        case onAppear
         
+        // MARK: - Update State
+        case fetchGoalsCompleted([GoalEditCardItem])
     }
     
+    /// 외부에서 주입한 Reduce로 EditGoalReducer를 구성합니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// let reducer = EditGoalReducer(
+    ///     reducer: Reduce { _, _ in .none }
+    /// )
+    /// ```
     public init(reducer: Reduce<State, Action>) {
         self.reducer = reducer
     }
@@ -35,4 +82,3 @@ public struct EditGoalReducer {
         reducer
     }
 }
-
--- Projects/Feature/Home/Sources/Goal/EditGoalReducer+Impl.swift
+++ Projects/Feature/Home/Sources/Goal/EditGoalReducer+Impl.swift
@@ -8,12 +8,47 @@
 import Foundation
 
 import ComposableArchitecture
+import DomainGoalInterface
 import FeatureHomeInterface
+import SharedDesignSystem
 
 extension EditGoalReducer {
+    /// 실제 로직을 포함한 EditGoalReducer를 생성합니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// let reducer = EditGoalReducer()
+    /// ```
+    // swiftlint:disable:next function_body_length
     public init() {
+        @Dependency(\.goalClient) var goalClient
+        
+        // swiftlint:disable:next closure_body_length
         let reducer = Reduce<State, Action> { state, action in
-            return .none
+            switch action {
+                // MARK: - LifeCycle
+            case .onAppear:
+                return .run { send in
+                    let goals = try await goalClient.fetchGoals()
+                    let items = goals.map { goal in
+                        // FIXME: - Goal Entity 변경
+                        GoalEditCardItem(
+                            id: goal.id,
+                            goalName: goal.title,
+                            iconImage: goal.goalIcon,
+                            repeatCycle: "미정",
+                            startDate: "-",
+                            endDate: "미설정"
+                        )
+                    }
+                    await send(.fetchGoalsCompleted(items))
+                }
+                
+                // MARK: - Update State
+            case let .fetchGoalsCompleted(items):
+                state.cards = items
+                return .none
+            }
         }
         
         self.init(reducer: reducer)
