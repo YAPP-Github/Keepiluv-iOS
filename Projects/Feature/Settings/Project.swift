70a403fa1292af9e7d531ea41f5b52a8b0cce274
Jiyong Jung <hickerd@sch.ac.kr>
2026-02-06 21:04:51 +0900

    feat: add Settings feature interface - #105

---
+++ Projects/Feature/Settings/Project.swift
@@ -0,0 +1,42 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+    name: Module.Feature.name + Module.Feature.settings.rawValue,
+    targets: [
+        .feature(
+            interface: .settings,
+            config: .init(
+                dependencies: [
+                    .external(dependency: .ComposableArchitecture),
+                    .shared(implements: .util),
+                    .shared(implements: .designSystem)
+                ]
+            )
+        ),
+        .feature(
+            implements: .settings,
+            config: .init(
+                dependencies: [
+                    .feature(interface: .settings),
+                    .shared(implements: .designSystem),
+                    .shared(implements: .util),
+                    .external(dependency: .ComposableArchitecture)
+                ]
+            )
+        ),
+        .feature(
+            example: .settings,
+            config: .init(
+                infoPlist: .extendingDefault(with: [
+                    "UIUserInterfaceStyle": "Light"
+                ]),
+                dependencies: [
+                    .feature(implements: .settings),
+                    .shared(implements: .designSystem),
+                    .external(dependency: .ComposableArchitecture)
+                ]
+            )
+        )
+    ]
+)
