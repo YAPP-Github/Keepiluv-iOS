1ed7d2bc2f236e0b5a0f410e837190d7ad0bf1d1
jihun <jihun332@gmai.com>
2026-02-07 00:19:56 +0900

    feat: Icon, Emoji Image 매칭 - #109

---
+++ Projects/Feature/Home/Sources/Home/GoalReaction+Image.swift
@@ -0,0 +1,30 @@
+//
+//  GoalReaction+Image.swift
+//  FeatureHome
+//
+//  Created by Codex on 2/6/26.
+//
+
+import SwiftUI
+
+import DomainGoalInterface
+import SharedDesignSystem
+
+extension Goal.Reaction {
+    var image: Image {
+        switch self {
+        case .happy:
+            return .Icon.Illustration.happy
+        case .trouble:
+            return .Icon.Illustration.trouble
+        case .love:
+            return .Icon.Illustration.love
+        case .doubt:
+            return .Icon.Illustration.doubt
+        case .fuck:
+            return .Icon.Illustration.fuck
+        case .heart:
+            return .Icon.Illustration.heart
+        }
+    }
+}
