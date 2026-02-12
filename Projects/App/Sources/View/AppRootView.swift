f6dfe56e35fe47bf992011e95e122adc3b14e60a
jihun <jihun332@gmai.com>
2026-02-12 14:16:08 +0900

    feat: Navigation swipe - #158

--- Projects/App/Sources/View/AppRootView.swift
+++ Projects/App/Sources/View/AppRootView.swift
@@ -87,3 +87,14 @@ private extension AppRootView {
         )
     )
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
