96ce38b2831deb0683f46ce7ef3e716ca983e1c3
jihun <jihun332@gmai.com>
2026-02-19 20:28:51 +0900

    feat: StatsCoordinator View, Reducer 뼈대 구현 - #167

---
+++ Projects/Feature/Stats/Sources/Coordinator/StatsCoordinator+Impl.swift
@@ -0,0 +1,48 @@
+//
+//  StatsCoordinator+Impl.swift
+//  FeatureStats
+//
+//  Created by 정지훈 on 2/19/26.
+//
+
+import Foundation
+
+import ComposableArchitecture
+import FeatureStatsInterface
+
+extension StatsCoordinator {
+    /// 기본 구성의 StatsCoordinator를 생성합니다.
+    ///
+    /// ## 사용 예시
+    /// ```swift
+    /// let coordinator = StatsCoordinator(
+    ///     statsReducer: .init(),
+    ///     statsDetailReducer: .init()
+    /// )
+    /// ```
+    public init(
+        statsReducer: StatsReducer,
+        statsDetailReducer: StatsDetailReducer
+    ) {
+        let reducer = Reduce<State, Action> { state, action in
+            switch action {
+                // MARK: - Child Action
+            case .stats:
+                return .none
+
+            case .detail:
+                return .none
+
+                // MARK: - Binding
+            case .binding:
+                return .none
+            }
+        }
+        
+        self.init(
+            statsReducer: statsReducer,
+            statsDetailReducer: statsDetailReducer,
+            reducer: reducer
+        )
+    }
+}
