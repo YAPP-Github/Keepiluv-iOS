0e48b02bb7e7c65d9d33c39a8acad5021219d9d7
jihun <jihun332@gmai.com>
2026-04-20 20:45:46 +0900

    refactor: 공통 타입들 DomainCommon, FeatureCommon으로 책임 분리 - #278

--- Projects/Feature/Common/Interface/Sources/RepeatCycle+Text.swift
+++ Projects/Feature/Common/Interface/Sources/RepeatCycle+Text.swift
@@ -8,7 +8,11 @@
 import DomainCommonInterface
 
 public extension RepeatCycle {
-    public var text: String {
+    /// 반복 주기의 현재 표시 문구를 반환합니다.
+    ///
+    /// 공통 도메인 값을 여러 Feature에서 같은 방식으로 보여주기 위한
+    /// 단기 표시 정책이며, 실제 문자열 소유 책임은 Domain이 아니라 Feature에 둡니다.
+    var text: String {
         switch self {
         case .daily: "매일"
         case .weekly: "매주"
