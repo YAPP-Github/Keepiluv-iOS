607098a6b68d31fca08d416db5dd1c41c4926b34
jihun <jihun332@gmai.com>
2026-05-09 11:29:39 +0900

    refactor: CoreAnalytics 구조 개선 - #281 - CoreAnalytics에서 Feature책임을 갖고있던 기존 구조에서 Evetn를 protocol로 추상화 - logEvent 클로저로 통합

--- Projects/Core/Analytics/Interface/Sources/AnalyticsEvent.swift
+++ Projects/Core/Analytics/Interface/Sources/AnalyticsEvent.swift
@@ -8,16 +8,6 @@
 import Foundation
 
 /// 분석 도구로 전송할 이벤트가 따라야 하는 공통 인터페이스입니다.
-///
-/// ## 사용 예시
-/// ```swift
-/// enum HomeAnalyticsEvent: AnalyticsEvent {
-///     case homeViewed
-///
-///     var name: String { "home_viewed" }
-///     var parameters: [String: Any]? { nil }
-/// }
-/// ```
 public protocol AnalyticsEvent {
     var name: String { get }
     var parameters: [String: Any]? { get }
