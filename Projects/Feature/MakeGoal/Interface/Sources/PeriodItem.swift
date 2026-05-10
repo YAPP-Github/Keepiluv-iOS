baa8e63c2410a703c20c33844fb28f98e162acb5
jihun <jihun332@gmai.com>
2026-05-10 21:27:54 +0900

    refactor: RepeatCycle+Text재활용 리뷰 반영 - #278

--- Projects/Feature/MakeGoal/Interface/Sources/PeriodItem.swift
+++ Projects/Feature/MakeGoal/Interface/Sources/PeriodItem.swift
@@ -5,6 +5,8 @@
 //  Created by 정지훈 on 4/20/26.
 //
 
+import DomainCommonInterface
+import FeatureCommonInterface
 import SharedDesignSystem
 
 /// 목표 생성/수정 화면에서 사용하는 반복 주기 탭 아이템입니다.
@@ -14,10 +16,22 @@ public enum PeriodItem: TXItem {
     case monthly
 
     public var title: String {
+        repeatCycle.text
+    }
+
+    public var repeatCycle: RepeatCycle {
         switch self {
-        case .daily: return "매일"
-        case .weekly: return "매주"
-        case .monthly: return "매월"
+        case .daily: return .daily
+        case .weekly: return .weekly
+        case .monthly: return .monthly
+        }
+    }
+
+    public init(repeatCycle: RepeatCycle) {
+        switch repeatCycle {
+        case .daily: self = .daily
+        case .weekly: self = .weekly
+        case .monthly: self = .monthly
         }
     }
 }
